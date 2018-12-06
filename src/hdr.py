204d7a3b699e4a61b13277e98fc7cfb286293370
代超 <daichao@paletress754.local>
2018-12-06 15:14:39 +0800

    new

--- src/hdr.py
+++ src/hdr.py
@@ -11,43 +11,47 @@ from src.tonemap import *
 
 
 def Show_origin_and_output(origin, I):
-   """
-   Show final result.
-   """
-   plt.figure(figsize=(12, 6))
-   plt.subplots_adjust(left=0,right=1,bottom=0,top=1, wspace=0.005, hspace=0)
+	 """
+	 Show final result.
+	 """
+	 plt.figure(figsize=(12, 6))
+	 plt.subplots_adjust(left=0,right=1,bottom=0,top=1, wspace=0.005, hspace=0)
 
-   plt.subplot(121),plt.imshow(np.flip(origin, 2)),plt.title('Origin')
-   plt.axis('off')
-   plt.subplot(122),plt.imshow(np.flip(I, 2)),plt.title('Fake HDR')
-   plt.axis('off')
-   plt.savefig('compare.png', bbox_inches='tight', pad_inches=0)
-   plt.show()
-   
+	 plt.subplot(121),plt.imshow(np.flip(origin, 2)),plt.title('Origin')
+	 plt.axis('off')
+	 plt.subplot(122),plt.imshow(np.flip(I, 2)),plt.title('Fake HDR')
+	 plt.axis('off')
+	 plt.savefig('compare.png', bbox_inches='tight', pad_inches=0)
+	 plt.show()
+	 
 
 
 class FakeHDR():
-    
-    def __init__(self, flag):
-        self.weighted_fusion = flag
-        self.wls = wlsFilter
-        self.srs = SRS
-        self.vig = VIG
-        self.tonemap = tonereproduct
+	def __init__(self, flag):
+		self.weighted_fusion = flag
+		self.wls = wlsFilter
+		self.srs = SRS
+		self.vig = VIG
+		self.tonemap = tonereproduct
 
-    def process(self, image):
-        
-        if image.shape[2] == 4:
-            image = image[:,:,0:3]
-        S = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)/255.0
-        image = 1.0*image/255
-        L = 1.0*S
-    
-        I = self.wls(S)
-        R = np.log(L+1e-22) - np.log(I+1e-22)
-        R_ = self.srs(R, L)
-        I_K = self.vig(L, 1.0 - L)
-
-        result_ = self.tonemap(image, L, R_, I_K, self.weighted_fusion)
-        return result_
+	def process(self, image):
+		if image.shape[2] == 4:
+				image = image[:,:,0:3]
+		S = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)/255.0
+		image = 1.0*image/255
+		L = 1.0*S
+		cv2.imwrite("original.png", L*255.0)
+		I = self.wls(S)
+		cv2.imwrite("illuminance.png", I*255.0)
+		R = np.log(L+1e-22) - np.log(I+1e-22)
+		cv2.imwrite("reflectance.png", R*255.0)
+		R_ = self.srs(R, L)
+		cv2.imwrite("reflectance_srs.png", R_*255.0)
+		I_K = self.vig(L, 1.0 - L)
+		k = 1
+		for img in I_K:
+			cv2.imwrite("vig"+str(k)+".png", img*255.0)
+			k=k+1
+		result_ = self.tonemap(image, L, R_, I_K, self.weighted_fusion)
+		return result_
 
